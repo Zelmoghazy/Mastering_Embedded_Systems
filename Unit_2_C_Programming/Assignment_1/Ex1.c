6d0603c8629f184dd881d0201eb6f8016d93f0fa
zelmoghazy <zelmoghazy@gmail.com>
2023-07-02 19:36:13 +0300

    Edits

--- Unit_2_C_Programming/Assignment_1/Ex1.c
+++ Unit_2_C_Programming/Assignment_1/Ex1.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void print_with_padding(const char *str){
-    const char *Console = "Console-output";       
-    const char *padding = "##################################################";
-    size_t targetStrLen = strlen(padding);                 
+void repeatSymbol(char str[], size_t size, char symbol) {
+    size_t i;
+    for (i = 0; i < size; i++)
+        str[i] = symbol;
+    str[size]='\0';
+}
 
-    size_t padLen = targetStrLen - strlen(Console);  
-
-    printf("%*.*s%s%*.*s\n", padLen / 2, padLen / 2, padding, Console, (padLen + 1) / 2, (padLen + 1) / 2, padding);
+void print_with_padding(const char *str, const char *title, size_t size, char symbol){
+    const char *Label = title;
+    char *Padding = malloc(sizeof(char)*size+1);
+    repeatSymbol(Padding,size,symbol);       
+    size_t Length = size - strlen(Label);  
+    printf("%*.*s%s%*.*s\n", Length / 2, Length / 2, Padding, Label, (Length + 1) / 2, (Length + 1) / 2, Padding);
     printf("%s\n",str);
-    printf("%s\n",padding);
+    printf("%s\n",Padding);
+    free(Padding);
 }
 
 int main() {
-    print_with_padding("C Programming");
+    print_with_padding("C Programming","Console-output",50,'#');
+    system("pause");
     return 0;
 }
