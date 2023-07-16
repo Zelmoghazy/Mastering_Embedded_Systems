eea74493183b0dca6c70e83483b56a0d6d5b7e59
Zeyad Ahmed Ibrahim <99360533+Zelmoghazy@users.noreply.github.com>
2023-07-16 06:58:23 +0300

    Typo

--- Unit_2_C_Programming/Midterm_1/Ex2.c
+++ Unit_2_C_Programming/Midterm_1/Ex2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* c function to take an integer number and calculate it's square root */
-double squareRoot(int number, int percision)
+double squareRoot(int number, int precision)
 {
     if (number < 0){
         printf("Error : square root of negative number doesnt exist!\n");
@@ -29,7 +29,7 @@ double squareRoot(int number, int percision)
         }
     }
     double increment = 0.1;
-    for (i= 0; i < percision; i++)
+    for (i= 0; i < precision; i++)
     {
         while (result * result <= number){
             result += increment;
