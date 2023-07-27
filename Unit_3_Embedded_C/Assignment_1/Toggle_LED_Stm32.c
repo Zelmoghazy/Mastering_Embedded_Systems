ae69303a5061e11bbb840f30c78d2bc6e99cb94b
zelmoghazy <zelmoghazy@gmail.com>
2023-07-27 09:14:09 +0300

    Another technique

--- Unit_3_Embedded_C/Assignment_1/Toggle_LED_Stm32.c
+++ Unit_3_Embedded_C/Assignment_1/Toggle_LED_Stm32.c
@@ -17,6 +17,48 @@
     mask = ~mask;                      \
     a &= mask
 
+typedef union R_ODR_t {
+    vuint32_t all_fields;
+    struct{
+        vuint32_t pin0 :1;
+        vuint32_t pin1 :1;
+        vuint32_t pin2 :1;
+        vuint32_t pin3 :1;
+        vuint32_t pin4 :1;
+        vuint32_t pin5 :1;
+        vuint32_t pin6 :1;
+        vuint32_t pin7 :1;
+        vuint32_t pin8 :1;
+        vuint32_t pin9 :1;
+        vuint32_t pin10:1;
+        vuint32_t pin11:1;
+        vuint32_t pin12:1;
+        vuint32_t pin13:1;
+        vuint32_t pin14:1;
+        vuint32_t pin15:1;
+        vuint32_t pin16:1;
+        vuint32_t pin17:1;
+        vuint32_t pin18:1;
+        vuint32_t pin19:1;
+        vuint32_t pin20:1;
+        vuint32_t pin21:1;
+        vuint32_t pin22:1;
+        vuint32_t pin23:1;
+        vuint32_t pin24:1;
+        vuint32_t pin25:1;
+        vuint32_t pin26:1;
+        vuint32_t pin27:1;
+        vuint32_t pin28:1;
+        vuint32_t pin29:1;
+        vuint32_t pin30:1;
+        vuint32_t pin31:1;
+    }pin;
+
+}R_ODR_t;
+
+volatile R_ODR_t* R_ODR = (volatile R_ODR_t*)(GPIO_A_ODR);
+
+
 int main(void)
 {
     SET(RCC_APB2ENR,2);
@@ -25,9 +67,11 @@ int main(void)
     
     for (;;)
     {
-        SET(GPIO_A_ODR,13);
+        // SET(GPIO_A_ODR,13);
+        R_ODR->pin.pin13 = 1;
         /* Delay */
-        CLEAR(GPIO_A_ODR,13);
+        // CLEAR(GPIO_A_ODR,13);
+        R_ODR->pin.pin13 = 0;
         /* Delay */
     }
     
