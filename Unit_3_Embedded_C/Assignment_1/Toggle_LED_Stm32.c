3b23cf4b7a142b1acc1ab9a9d900116aebfd96e5
zelmoghazy <zelmoghazy@gmail.com>
2023-07-30 00:51:25 +0300

    Edit

--- Unit_3_Embedded_C/Assignment_1/Toggle_LED_Stm32.c
+++ Unit_3_Embedded_C/Assignment_1/Toggle_LED_Stm32.c
@@ -1,8 +1,21 @@
+/******************************************************************************
+ *  - Base address RCC -> 0x40021000                                          *
+ *     - The RCC is necessary because the GPIO has disabled clock by default. *
+ *     - APB2ENR -> 0x18 Offset                                               *
+ *     - IOPAEN  -> 2nd bit                                                   *
+ *                                                                            *
+ *  - GPIO Port A -> 0x40010800                                               *
+ *      - CRH -> 0x04 offset                                                  *
+ *      - ODR -> ox0C offset                                                  *
+ ******************************************************************************/
+
+
 #include "Platform_Types.h"
 
 #define RCC_BASE    0x40021000
 #define PORT_A_BASE 0x40010800
-#define RCC_APB2ENR ((*(vuint32_t *) (RCC_BASE + 0x18)))
+
+#define RCC_APB2ENR ((*(vuint32_t *) (RCC_BASE    + 0x18)))
 #define GPIO_A_CRH  ((*(vuint32_t *) (PORT_A_BASE + 0x04)))
 #define GPIO_A_ODR  ((*(vuint32_t *) (PORT_A_BASE + 0x0C)))
 
@@ -11,11 +24,13 @@
 #define TOGGLE(a,n) ((a) ^=  (1 << (n)))
 
 #define CLEAR_RANGE(a, s, e)           \
+    do{                                \
     uint32 mask = ~0;                  \
     mask = mask >> (32 - (e - s + 1)); \
     mask = mask << s;                  \
     mask = ~mask;                      \
-    a &= mask
+    a &= mask;                         \
+    }while(0)
 
 typedef union R_ODR_t {
     vuint32_t all_fields;
@@ -53,16 +68,16 @@ typedef union R_ODR_t {
         vuint32_t pin30:1;
         vuint32_t pin31:1;
     }pin;
-
 }R_ODR_t;
 
 volatile R_ODR_t* R_ODR = (volatile R_ODR_t*)(PORT_A_BASE + 0x0C);
 
-
 int main(void)
 {
-    SET(RCC_APB2ENR,2);
+    SET(RCC_APB2ENR,2); //RCC_IOPAEN
+
     CLEAR_RANGE(GPIO_A_CRH,20,23);
+
     SET(GPIO_A_CRH,21);
     
     for (;;)
@@ -74,6 +89,4 @@ int main(void)
         R_ODR->pin.pin13 = 0;
         /* Delay */
     }
-    
-    return 0;
 }
