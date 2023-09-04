3059d422a0595897ec46f3fbade2d8a180ab122d
zelmoghazy <zelmoghazy@gmail.com>
2023-09-04 20:23:05 +0300

    Misra Rules

--- Unit_4_System_Architecture/Data_Structures/FIFO/run.bat
+++ Unit_4_System_Architecture/Data_Structures/FIFO/run.bat
@@ -1,1 +1,3 @@
 @make && cd .\build && .\Main.exe && cd ..
+cppcheck --dump quiet .\FIFO.c
+python "C:\Program Files\Cppcheck\addons\misra.py" --rule-texts="C:\Program Files\Cppcheck\addons\misra_rules.txt" .\FIFO.c.dump
