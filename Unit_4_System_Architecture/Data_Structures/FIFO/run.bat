f73ee8d43fdf6367d528b67212a7e9df9b772809
zelmoghazy <zelmoghazy@gmail.com>
2023-09-10 18:12:55 +0300

    Cleanup

--- Unit_4_System_Architecture/Data_Structures/FIFO/run.bat
+++
@@ -1,3 +0,0 @@
-@make && cd .\build && .\Main.exe && cd ..
-cppcheck --dump quiet .\FIFO.c
-python "C:\Program Files\Cppcheck\addons\misra.py" --rule-texts="C:\Program Files\Cppcheck\addons\misra_rules.txt" .\FIFO.c.dump
