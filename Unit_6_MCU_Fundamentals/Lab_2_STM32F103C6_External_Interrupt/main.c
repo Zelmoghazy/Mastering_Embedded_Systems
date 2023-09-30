756e4c077a57497201b3df3e004b44dfc2de6514
zelmoghazy <zelmoghazy@gmail.com>
2023-10-01 02:27:42 +0300

    Lab_2 Done

--- Unit_6_MCU_Fundamentals/Lab_2_STM32F103C6_External_Interrupt/main.c
+++ Unit_6_MCU_Fundamentals/Lab_2_STM32F103C6_External_Interrupt/main.c
@@ -7,8 +7,20 @@
 #define RCC_CR      ((*(vuint32_t *) (RCC_BASE + 0x0)))
 
 #define PORT_A_BASE 0x40010800
+#define GPIO_A_CRL  ((*(vuint32_t *) (PORT_A_BASE + 0x00)))
 #define GPIO_A_CRH  ((*(vuint32_t *) (PORT_A_BASE + 0x04)))
 #define GPIO_A_ODR  ((*(vuint32_t *) (PORT_A_BASE + 0x0C)))
+
+#define EXTI_BASE  0x40010400
+#define EXTI_IMR   ((*(vuint32_t *) (EXTI_BASE + 0x00)))
+#define EXTI_RTSR  ((*(vuint32_t *) (EXTI_BASE + 0x08)))
+#define EXTI_PR    ((*(vuint32_t *) (EXTI_BASE + 0x14)))
+
+#define NVIC_ISER0 ((*(vuint32_t *) (0xE000E100)))
+
+#define AFIO_BASE    0x40010000
+#define AFIO_EXTICR1 ((*(vuint32_t *) (AFIO_BASE  + 0x08)))
+
 
 
 typedef union R_ODR_t {
@@ -69,20 +81,46 @@ void init_GPIO()
     // Set pin 13 port A as Output
     CLEAR_RANGE(GPIO_A_CRH,20,23);
     SET(GPIO_A_CRH,21);
+
+    // Set Port A pin 0 as Floating input
+    SET(GPIO_A_CRL,2);
+}
+
+
+void init_EXTI()
+{
+    // Enable AFIO Clock
+    SET(RCC_APB2ENR,0);
+
+    // EXTI 0 configuration
+    AFIO_EXTICR1 = 0;
+    // Enable EXTI Line 0 (Mask 1)
+    SET(EXTI_IMR,0);
+    // Rising Trigger Enabled
+    SET(EXTI_RTSR,0);
+    // Enable NVIC IRQ6 >> EXTI0
+    SET(NVIC_ISER0,6);
 }
 
 int main(void)
 {
     init_clock();
     init_GPIO();
+    init_EXTI();
 
     for (;;)
     {
-        R_ODR->pin.pin13 = 1;
-        for(int i = 0; i < 1000; i++);
-        
-        R_ODR->pin.pin13 = 0;
-        for(int i = 0; i < 1000; i++);
+
     }
     return 0;
+}
+
+void EXTI0_IRQHandler(void)
+{
+    // IRQ Occured EXTI0 >>> PORTA_Pin 0 >>> Edge detection : rising edge
+    // Toggle LED pin 13 GPIO_A
+    R_ODR->pin.pin13 ^= 1;
+    // Clear pending request for line 0
+    SET(EXTI_PR,0);
+    
 }
