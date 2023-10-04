5acae7b0bcee3f040989e34dca1393ae9e717dfa
zelmoghazy <zelmoghazy@gmail.com>
2023-10-04 17:38:56 +0300

    Add comments

--- Unit_6_MCU_Fundamentals/Lab_3_ATMEGA32_External_Interrupt/main.c
+++ Unit_6_MCU_Fundamentals/Lab_3_ATMEGA32_External_Interrupt/main.c
@@ -7,20 +7,30 @@
 #define IO_BASE    0x20
 #define IO_PORT_D  ((*(vuint32_t *) (IO_BASE  + 0x12)))
 #define IO_DDRD    ((*(vuint32_t *) (IO_BASE  + 0x11)))
-#define INT_MCUCR  ((*(vuint32_t *) (IO_BASE  + 0x35)))
-#define INT_GICR   ((*(vuint32_t *) (IO_BASE  + 0x3B)))
-#define INT_MCUCSR ((*(vuint32_t *) (IO_BASE  + 0x34)))
+#define INT_MCUCR  ((*(vuint32_t *) (IO_BASE  + 0x35)))  // MCU Control Register
+#define INT_MCUCSR ((*(vuint32_t *) (IO_BASE  + 0x34)))  // MCU Control and Status Register
+#define INT_GICR   ((*(vuint32_t *) (IO_BASE  + 0x3B)))  // General interrupt Control Register
 
 int main(void)
 {
+    /* Set Pin 5 and Pin 6 and Pin 7 to Output */
     SET_MASK(IO_DDRD,0b111,5);
-    SET(INT_MCUCR,0);
-    CLEAR(INT_MCUCR,1);
-    SET(INT_MCUCR,2);
-    SET(INT_MCUCR,3);
+
+    /* Any Logical change on INT0 generates an interrupt request */
+    SET_MASK(INT_MCUCR,0b01,0);
+
+    /* The rising edge of INT1 generates an interrupt request */
+    SET_MASK(INT_MCUCR,0b11,2);
+
+    /* The falling edge of INT2 generates an interrupt request */
     CLEAR(INT_MCUCSR,6);
+
+    // External Interrupt Request Enable
     SET_MASK(INT_GICR,0b111,5);
+
+    // Global interrupt enable 
     sei();
+
     for(;;)
     {
         CLEAR(IO_PORT_D,5);
@@ -33,15 +43,15 @@ int main(void)
 ISR(INT0_vect)
 {
     SET(IO_PORT_D,5);
-    _delay_ms(1000);
+    _delay_ms(100);
 }
 ISR(INT1_vect)
 {
     SET(IO_PORT_D,6);
-    _delay_ms(1000);
+    _delay_ms(100);
 }
 ISR(INT2_vect)
 {
     SET(IO_PORT_D,7);
-    _delay_ms(1000);
+    _delay_ms(100);
 }
